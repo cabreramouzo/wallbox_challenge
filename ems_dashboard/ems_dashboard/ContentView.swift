8318d9663c75cf3e0c906e1b4af3969330348009
MAC <miguelangelcabrera1@hotmail.com>
2022-05-23 19:47:36 +0200

    Separate charts

--- ems_dashboard/ems_dashboard/ContentView.swift
+++ ems_dashboard/ems_dashboard/ContentView.swift
@@ -12,112 +12,111 @@ struct ContentView: View {
     @StateObject var HistoricalDataVM = HistoricalDataViewModel()
     var body: some View {
         NavigationView {
-        VStack {
-            Form {
-                Section(header: Text("Quasar Energy")) {
-                    HStack {
-                        VStack {
-                            Text(String(liveDataVM.liveData.quasars_power))
-                            Text("Charged")
+            VStack {
+                Form {
+                    Section(header: Text("Quasar Energy")) {
+                        HStack {
+                            VStack {
+                                Text(String(liveDataVM.liveData.quasars_power))
+                                Text("Charged")
+                            }
+                            Spacer()
+                            VStack {
+                                Text(String(liveDataVM.liveData.quasars_power))
+                                Text("Discharged")
+                            }
+                            
                         }
-                        Spacer()
-                        VStack {
-                            Text(String(liveDataVM.liveData.quasars_power))
-                            Text("Discharged")
-                        }
-                        
                     }
-                }
-                
-                Section(header: Text("Live data")) {
-                    HStack {
-                        VStack {
-                            Text(String(liveDataVM.liveData.solar_power))
-                            Text("Solar Power")
+                    
+                    Section(header: Text("Live data")) {
+                        HStack {
+                            VStack {
+                                Text(String(liveDataVM.liveData.solar_power))
+                                Text("Solar Power")
+                            }
+                            Spacer()
+                            VStack {
+                                Text(String(liveDataVM.liveData.quasars_power))
+                                Text("Quasar Power")
+                            }
+                            
                         }
-                        Spacer()
-                        VStack {
-                            Text(String(liveDataVM.liveData.quasars_power))
-                            Text("Quasar Power")
+                        HStack {
+                            VStack {
+                                Text(String(liveDataVM.liveData.grid_power))
+                                Text("Grid")
+                            }
+                            Spacer()
+                            VStack {
+                                Text(String(liveDataVM.liveData.system_soc))
+                                Text("System soc")
+                            }
+                            
                         }
-                        
+                        HStack {
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Total Energy")
+                            }
+                            Spacer()
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Current Energy")
+                            }
+                            
+                        }
                     }
-                    HStack {
-                        VStack {
-                            Text(String(liveDataVM.liveData.grid_power))
-                            Text("Grid")
+                    
+                    Section(header: Text("Statistics")) {
+                        NavigationLink(
+                            destination: detailView(
+                                samples_building: HistoricalDataVM.getReducedSampleValues(reduced: 10, by: .building_active_power),
+                                samples_grid: HistoricalDataVM.getReducedSampleValues(reduced: 10, by: .grid_active_power),
+                                samples_pv: HistoricalDataVM.getReducedSampleValues(reduced: 10, by: .pv_active_power),
+                                samples_quasars: HistoricalDataVM.getReducedSampleValues(reduced: 10, by: .quasars_active_power)),
+                            label: {
+                                Text("View chart details")
+                            })
+                            .navigationTitle(Text("View charts"))
+                        HStack {
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Solar Power")
+                            }
+                            Spacer()
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Quasar Power")
+                            }
+                            
                         }
-                        Spacer()
-                        VStack {
-                            Text(String(liveDataVM.liveData.system_soc))
-                            Text("System soc")
+                        HStack {
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Grid")
+                            }
+                            Spacer()
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("System soc")
+                            }
                         }
-                        
-                    }
-                    HStack {
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Total Energy")
+                        HStack {
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Total Energy")
+                            }
+                            Spacer()
+                            VStack {
+                                Text("3.45 Kwh")
+                                Text("Current Energy")
+                            }
+                            
                         }
-                        Spacer()
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Current Energy")
-                        }
-                        
-                    }
-                }
-                
-                Section(header: Text("Statistics")) {
-                    NavigationLink(
-                        destination: detailView(
-                            samples_building: HistoricalDataVM.getReducedSampleValues(reduced: 5, by: .building_active_power),
-                            samples_grid: HistoricalDataVM.getReducedSampleValues(reduced: 5, by: .grid_active_power),
-                            samples_pv: HistoricalDataVM.getReducedSampleValues(reduced: 5, by: .pv_active_power),
-                            samples_quasars: HistoricalDataVM.getReducedSampleValues(reduced: 5, by: .quasars_active_power)),
-                        label: {
-                            Text("gohgof")
-                        })
-                        .navigationTitle(Text("Random Users"))
-                    HStack {
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Solar Power")
-                        }
-                        Spacer()
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Quasar Power")
-                        }
-                        
-                    }
-                    HStack {
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Grid")
-                        }
-                        Spacer()
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("System soc")
-                        }
-                        
-                    }
-                    HStack {
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Total Energy")
-                        }
-                        Spacer()
-                        VStack {
-                            Text("3.45 Kwh")
-                            Text("Current Energy")
-                        }
-                        
                     }
                 }
             }
-        }
         }
     }
 }
--- ems_dashboard/ems_dashboard/detailView.swift
+++ ems_dashboard/ems_dashboard/detailView.swift
@@ -14,18 +14,23 @@ struct detailView: View {
     var samples_pv: [Double]
     var samples_quasars: [Double]
     
+    let chart_style_building = ChartStyle(backgroundColor: Color.white, accentColor: Color.black, gradientColor: GradientColors.green, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: Color.black)
+    let chart_style_grid = ChartStyle(backgroundColor: Color.white, accentColor: Color.black, gradientColor: GradientColors.purple, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: Color.black)
+    let chart_style_pv = ChartStyle(backgroundColor: Color.white, accentColor: Color.black, gradientColor: GradientColors.orngPink, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: Color.black)
+    let chart_style_quasars = ChartStyle(backgroundColor: Color.white, accentColor: Color.black, gradientColor: GradientColors.prplNeon, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: Color.black)
+    
     var body: some View {
 
         ScrollView {
-            LineView(data: samples_building, title: "Building").padding().frame(width: .infinity, height: 300, alignment: .center)
+            LineView(data: samples_building, title: "Building", style: chart_style_building).padding().frame(width: .infinity, height: 300, alignment: .center)
             Spacer().padding(.bottom, 50)
-                LineView(data: samples_grid, title: "Grid").padding()
+                LineView(data: samples_grid, title: "Grid", style: chart_style_grid).padding()
                     .frame(width: .infinity, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Spacer().padding(.bottom, 50)
-                LineView(data: samples_pv, title: "PV").padding()
+                LineView(data: samples_pv, title: "PV", style: chart_style_pv).padding()
                     .frame(width: .infinity, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Spacer().padding(.bottom, 50)
-                LineView(data: samples_quasars, title: "Quasars").padding()
+                LineView(data: samples_quasars, title: "Quasars", style: chart_style_quasars).padding()
                     .frame(width: .infinity, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Spacer().padding(.bottom, 50)
             MultiLineChartView(data: [
@@ -34,14 +39,8 @@ struct detailView: View {
                                 (samples_pv, GradientColors.orngPink),
                                 (samples_quasars, GradientColors.prplNeon)], title: "All", legend: "", style: ChartStyle(backgroundColor: Color.white, accentColor: Color.black, gradientColor: GradientColors.green, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: Color.clear), form: ChartForm.large, dropShadow: false)
             Spacer().padding(.bottom, 50)
-            
-            
-            
-            
+
         }
-            
-        
-        
     }
 }
 
